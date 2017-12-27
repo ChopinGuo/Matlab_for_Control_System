f8b30a8db3a232c50143e22e14101bfc2fea4b4a
ChopinGuo <guoshaopan@pku.edu.cn>
2017-12-27 19:55:34 +0800

    Add Ex19_24

--- Ch19_6.m
+++ Ch19_6.m
@@ -1,20 +1,50 @@
 % Ex19_23
 
-A = [0 1; 2 -1];
-d = eig(A);
+% A = [0 1; 2 -1];
+% d = eig(A);
+% 
+% Q = eye(2);
+% 
+% syms p11 p12 p21 p22;
+% P = [p11 p12; p21 p22];
+% FQ = -Q;
+% APPA = A'*P + P*A;
+% 
+% if det(A)~=0
+%     [p11, p12, p22] = solve('4*p12=-1', 'p11-p12+2*p22=0','2*p12-2*p22=-1');
+%     p21 = p12;
+%     P = [p11 p12; p21 p22]
+%     detp11 = det(P(1,1))
+%     detP = det(P)
+% end
+
+% Ex19_24
+
+A = [-1 1; 2 -3];
+if det(A)~=0
+    disp('A is nosingular,and the origin is the only equilibrium state.')
+else
+    disp('A is singular.')
+end
 
 Q = eye(2);
 
-syms p11 p12 p21 p22;
+syms p11 p12 p21 p22 x1 x2;
 P = [p11 p12; p21 p22];
 FQ = -Q;
 APPA = A'*P + P*A;
 
 if det(A)~=0
-    [p11, p12, p22] = solve('4*p12=-1', 'p11-p12+2*p22=0','2*p12-2*p22=-1');
-    p21 = p12;
-    P = [p11 p12; p21 p22]
-    detp11 = det(P(1,1))
-    detP = det(P)
+    [p11 p12 p21 p22] = solve('2*p12 - 2*p11 + 2*p21 = -1',...
+        'p11 - 4*p12 + 2*p22 = 0',...
+        'p11 - 4*p21 + 2*p22 = 0',...
+        'p12 + p21 - 6*p22 = -1');
 end
 
+P = [p11 p12; p21 p22];
+detP11 = det(P(1,1))
+detP = det(P)
+
+x = [x1; x2];
+vPx = collect(x.'*P*x)
+vQx = x.' * (-Q) * x
